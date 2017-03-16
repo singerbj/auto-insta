77da565b57216bae8f81e94e482866fade34d803
Benjamin Singer <Benjamin.singer@helpsystems.com>
2017-03-16 14:57:35 -0500

    boop

---
+++ test.js
@@ -0,0 +1,44 @@
+/*global __dirname, catch*/
+
+(function () {
+    'use strict';
+
+    var Client = require('instagram-private-api').V1;
+    var user = 'bentesterman';
+    var password = process.env.autoinsta;
+    var device = new Client.Device(user);
+    var storage = new Client.CookieFileStorage(__dirname + '/cookies/' + user + '.json');
+    var q = require('q');
+    var rp = require('request-promise');
+
+
+    var postPicture = function (path, description) {
+        var deferred = q.defer();
+        Client.Session.create(device, storage, user, password).then(function (session) {
+            session.getAccount().then(function (account) {
+                // console.log(account.params);
+                Client.Upload.photo(session, path).then(function (upload) {
+                    // console.log(upload.params.uploadId);
+                    return Client.Media.configurePhoto(session, upload.params.uploadId, description);
+                }).then(function (medium) {
+                    // console.log(medium.params);
+                    deferred.resolve();
+                })['catch'](function (error) {
+                    deferred.reject(error);
+                });
+            })['catch'](function (error) {
+                deferred.reject(error);
+            });
+        })['catch'](function (error) {
+            deferred.reject(error);
+        });
+        return q.promise;
+    };
+
+    var getTodaysPicture = function () {
+
+    };
+
+    postPicture('./image.jpg', '\n#yolo\n#swag\n#dopeness');
+
+}());
