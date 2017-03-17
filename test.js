3d35d015db9dd89e3ff81bef1d2a2b476e82ad20
Benjamin Singer <Benjamin.singer@helpsystems.com>
2017-03-17 09:50:00 -0500

    updates yo

--- test.js
+++ test.js
@@ -1,64 +1,114 @@
-/*global __dirname, catch*/
+/*global __dirname, catch, process, console, */
 
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  var Client = require('instagram-private-api').V1;
-  var storage = new Client.CookieFileStorage(__dirname + '/cookies/' + user + '.json');
-  var q = require('q');
-  var request = require('request');
-  var rp = require('request-promise');
-  var fs = require('fs');
 
-  var user = 'bentesterman';
-  var password = process.env.autoinsta;
-  var device = new Client.Device(user);
-  var fileName = 'image.jpg';
+    var q = require('q');
+    var request = require('request');
+    var rp = require('request-promise');
+    var fs = require('fs');
 
-  var downloadImage = function(uri, filename, callback) {
-    request.head(uri, function(err, res, body) {
-      request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-    });
-  };
 
-  var getTodaysPicture = function() {
-    var deferred = q.defer();
-    rp('https://www.reddit.com/r/EarthPorn/top.json?sort=top&t=day&limit=1').then(function(data) {
-      var url = JSON.parse(data).data.children[0].data.url;
-      downloadImage(url, fileName, function() {
-        deferred.resolve();
-      });
-    }).catch(function(error) {
-      deferred.reject(error);
-    });
-    return deferred.promise;
-  };
+    // Setup
+    var user = 'bentesterman';
+    var password = process.env.autoinsta;
+    var hashtags = ["earth", "visualsoflife", "beautiful", "lifeofadventure", "live", "letsgosomewhere", "instagood", "wonderful_places", "natgeo", "roamtheplanet", "exploring", "keepitwild", "wildlifeplanet", "exploremore", "bestvacations", "beautifuldestinations", "ourplanetdaily", "travelstoke", "lonelyplanet", "earthofficial", "earthpix", "beautifulplaces", "earthfocus", "awesomeearth", "nature", "nakedplanet", "forest"];
 
-  var postPicture = function(path, description) {
-    var deferred = q.defer();
-    Client.Session.create(device, storage, user, password).then(function(session) {
-      session.getAccount().then(function(account) {
-        // console.log(account.params);
-        Client.Upload.photo(session, path).then(function(upload) {
-          console.log('Photo upload successful with id: ' + upload.params.uploadId);
-          return Client.Media.configurePhoto(session, upload.params.uploadId, description);
-        }).then(function(medium) {
-          // console.log(medium.params);
-          deferred.resolve();
-        })['catch'](function(error) {
-          deferred.reject(error);
+    if (!password) {
+        console.log('Password not set! Exiting...');
+        process.exit(1);
+    }
+
+    var createDirsIfNotExist = function () {
+        var dirs = ['./images', './cookies'];
+        dirs.forEach(function (dir) {
+            if (!fs.existsSync(dir)) {
+                fs.mkdirSync(dir);
+            }
         });
-      })['catch'](function(error) {
-        deferred.reject(error);
-      });
-    })['catch'](function(error) {
-      deferred.reject(error);
-    });
-    return q.promise;
-  };
+    };
 
-  getTodaysPicture().done(function() {
-    postPicture(fileName, '\n#earth\n#visualsoflife\n#beautiful\n#lifeofadventure\n#live\n#letsgosomewhere\n#instagood\n#wonderful_places\n#natgeo\n#roamtheplanet\n#exploring\n#keepitwild\n#wildlifeplanet\n#exploremore\n#bestvacations\n#beautifuldestinations\n#ourplanetdaily\n#travelstoke\n#lonelyplanet\n#earthofficial\n#earthpix\n#beautifulplaces\n#earthfocus\n#awesomeearth\n#nature\n#nakedplanet\n#forest');
-  });
+    createDirsIfNotExist();
+
+    // Download/Instagram Functions
+    var Client = require('instagram-private-api').V1;
+    var storage = new Client.CookieFileStorage(__dirname + '/cookies/' + user + '.json');
+    var device = new Client.Device(user);
+
+    var onError = function (error, deferred) {
+        console.error(error);
+        deferred.reject(error);
+    };
+
+    var downloadImage = function (uri, fileName, callback) {
+        request.head(uri, function (err, res, body) {
+            request(uri).pipe(fs.createWriteStream(fileName)).on('close', function () {
+                console.log('Photo downloaded: ' + fileName);
+                callback();
+            });
+        });
+    };
+
+    var getTodaysPicture = function (subReddit) {
+        var deferred = q.defer();
+        var url;
+        var fileName;
+        rp('https://www.reddit.com/r/' + subReddit + '/top.json?sort=top&t=day&limit=1').then(function (data) {
+            url = JSON.parse(data).data.children[0].data.url;
+            fileName = './images/' + subReddit + '.jpg';
+            console.log('Top photo found: ' + url);
+            //TODO: check if link already used
+            downloadImage(url, fileName, function () {
+                deferred.resolve({
+                    fileName: fileName,
+                    url: url
+                });
+            });
+        })['catch'](function (error) {
+            onError(error, deferred);
+        });
+        return deferred.promise;
+    };
+
+    var postPicture = function (path, description) {
+        var deferred = q.defer();
+        Client.Session.create(device, storage, user, password).then(function (session) {
+            session.getAccount().then(function (account) {
+                // console.log(account.params);
+                Client.Upload.photo(session, path).then(function (upload) {
+                    console.log('Photo and description upload successful with id: ' + upload.params.uploadId);
+                    return Client.Media.configurePhoto(session, upload.params.uploadId, description);
+                }).then(function (medium) {
+                    // console.log(medium.params);
+                    deferred.resolve();
+                })['catch'](function (error) {
+                    onError(error, deferred);
+                });
+            })['catch'](function (error) {
+                onError(error, deferred);
+            });
+        })['catch'](function (error) {
+            onError(error, deferred);
+        });
+        return q.promise;
+    };
+
+    var buildDescription = function (url) {
+        return '#' + hashtags.join(' #') + '\n\nsrc = ' + url;
+    };
+
+    // Execution
+    getTodaysPicture('EarthPorn').done(function (data) {
+        postPicture(data.fileName, buildDescription(data.url));
+    });
+
+    //TODO: follow people who follow/post with tags
+
+    //TODO: comment on people's posts from tags
+
+    //TODO: implement scheduling or run on cron
+
+    //TODO: logging to files/notifications of errors
 
 }());
